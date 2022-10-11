20f1006ea36df218e9c1ebcb855bae8bd0c10eb0
Stefano <stef.pro@hotmail.com>
2022-10-11 18:10:45 -0400

    added bottom navbar on menu page

--- lib/pages/menu/menuPage.dart
+++ lib/pages/menu/menuPage.dart
@@ -6,6 +6,45 @@ class MenuPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.red,
+        selectedItemColor: Colors.black,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            label: 'home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            label: 'home',
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(50),
+              child: Text(
+                'Menu',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.kalam(
+                  fontSize: 35,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
