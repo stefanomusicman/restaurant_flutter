007430a7b94f65927f96bbb9716ee6253e1dc2ac
Stefano <stef.pro@hotmail.com>
2022-10-19 21:19:07 -0400

    can now see items in menu with custom card widget

---
+++ lib/pages/menu/menuCard.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MenuCard extends StatelessWidget {
+  final String name;
+  final double price;
+  final String image;
+  // final double id;
+
+  const MenuCard({
+    super.key,
+    required this.name,
+    required this.price,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(35),
+      child: Container(
+        width: 300,
+        // height: 300,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(35),
+              child: Image.asset(image),
+            ),
+            SizedBox(
+                height: 80,
+                width: 200,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        name,
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.comfortaa(
+                          fontSize: 17,
+                        ),
+                      ),
+                      Text(
+                        price.toString(),
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.comfortaa(
+                          fontSize: 17,
+                        ),
+                      ),
+                    ]))
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/menu/menuList.dart
+++ lib/pages/menu/menuList.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './menuCard.dart';
 import './menuItems.dart'; // list of menu Items
 
 class MenuList extends StatefulWidget {
@@ -11,16 +12,23 @@ class MenuList extends StatefulWidget {
 class _MenuListState extends State<MenuList> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: menuItems.length,
-      itemBuilder: ((context, index) {
-        return Card(
-          child: ListTile(
-            title: Text(menuItems[index].name),
-          ),
-        );
-      }),
+    return Expanded(
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: menuItems.length,
+        itemBuilder: ((context, index) {
+          return MenuCard(
+            name: menuItems[index].name,
+            price: menuItems[index].price,
+            image: menuItems[index].image,
+          );
+          // return Card(
+          //   child: ListTile(
+          //     title: Text(menuItems[index].name),
+          //   ),
+          // );
+        }),
+      ),
     );
   }
 }
